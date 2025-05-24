2927ff32d3cbc69b223675a7d073596ceac0265a
Johanna Schaefer <johanna.2.schaefer@uni-konstanz.de>
2025-05-24 16:22:10 +0200

    data_acquisition file, should be equivalent to version from april 29 on master branch

--- data_acquisition.py
+++ data_acquisition.py
@@ -1,5 +1,8 @@
-from koda_file_processing import *
-from api_connection import *
+
+#from koda_file_processing import *
+#from api_connection import *
+
+import pandas as pd
 
 ## Global function to manage the download of 1 day of static and real time data,
 ## processing all the .pb files and returning a csv with the collected data
@@ -29,7 +32,8 @@ def my_read_df(input_path):
 def add_df_features(input_df):
     column_list = ['trip_id','stop_id','route_short_name','stop_name','stop_headsign' ,'stop_sequence_real', 'arrival_time_real',
                    'departure_time_real', 'arrival_delay','departure_delay',
-                   'departure_time_sched','arrival_time_sched','service_id','start_date','shape_dist_traveled'] ## Only get the subset of columns we want to use
+                   'departure_time_sched','arrival_time_sched','service_id','start_date','shape_dist_traveled',
+                   'stop_lat','stop_lon'] ## Only get the subset of columns we want to use
 
     input_df['trip_id'] = input_df['trip_id'].astype(str)
     input_df['stop_id'] = input_df['stop_id'].astype(str)
@@ -50,4 +54,49 @@ def add_df_features(input_df):
 
     ordered_df['speed'] = (ordered_df['distance_between_stops'] / 1000) / (ordered_df['travel_time'] / 3600)
 
-    return ordered_df
+    ordered_df['dwell_time'] = ordered_df['departure_time_real'] - ordered_df['arrival_time_real']
+
+
+    list_trip_id = hospital_trips(ordered_df)
+
+    df_annoying = ordered_df.loc[
+        (ordered_df['trip_id'].isin(list_trip_id))
+        & (
+                ~(ordered_df['stop_name'] == 'Södersjukhuset')
+                & ~((ordered_df['stop_name'] == 'Rosenlund') & (ordered_df['stop_headsign'] == 'Sickla udde'))
+                & ~((ordered_df['stop_name'] == 'Södra station') & (ordered_df['stop_headsign'] == 'Hornsberg'))
+        )
+        ]
+
+    df_annoying['stop_sequence_real'] = df_annoying.apply(
+        lambda row: row['stop_sequence_real'] - 1
+        if row['stop_headsign'] not in ['Sickla udde via Södersjukhuset', 'Hornsberg via Södersjukhuset']
+        else row['stop_sequence_real'],
+        axis=1
+    )
+
+    df_annoying['stop_headsign'] = df_annoying['stop_headsign'].apply(
+        lambda des: 'Sickla udde' if des == 'Sickla udde via Södersjukhuset' else (
+            'Hornsberg' if des == 'Hornsberg via Södersjukhuset' else des))
+
+
+    regular_trips_df = ordered_df[~ordered_df['trip_id'].isin(list_trip_id)]
+    df_clean = pd.concat([df_annoying, regular_trips_df])
+
+
+    return df_clean
+
+
+
+def annoying_sequences(u):
+    if u['stop_headsign'] not in ['Sickla udde via Södersjukhuset', 'Hornsberg via Södersjukhuset']:
+        u['stop_sequence_sched'] -= 1
+
+    return u['stop_sequence_sched']
+
+
+def hospital_trips(df):
+    return list(set(df[(df['stop_name'] == 'Södersjukhuset') | (
+        df['stop_headsign'].isin(['Sickla udde via Södersjukhuset', 'Hornsberg via Södersjukhuset']))]['trip_id']))
+
+
